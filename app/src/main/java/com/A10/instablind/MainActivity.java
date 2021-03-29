214b7e7b9ddd18d8150438b1fc092821388c440c
EM-Idriss <74628794+EM-Idriss@users.noreply.github.com>
2021-03-29 22:24:41 +0100

    Project init

---
+++ app/src/main/java/com/A10/instablind/MainActivity.java
@@ -0,0 +1,14 @@
+package com.A10.instablind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
