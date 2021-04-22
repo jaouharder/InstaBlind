f4657fb0026291f1ef3960ec4ee43f4847138d24
MohammedOuttaleb <outaleb245@gmail.com>
2021-04-22 23:18:48 +0000

    implemented register and login process with firebase and changed the design

--- app/src/main/java/com/A10/instablind/MainActivity.java
+++ app/src/main/java/com/A10/instablind/MainActivity.java
@@ -1,130 +1,79 @@
 package com.A10.instablind;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.MenuItem;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.A10.instablind.Fragments.HomeFragment;
+import com.A10.instablind.Fragments.NotificationFragment;
+import com.A10.instablind.Fragments.ProfileFragment;
+import com.A10.instablind.Fragments.SearchFragment;
+import com.A10.instablind.PostActivity;
+import com.A10.instablind.R;
 
 public class MainActivity extends AppCompatActivity {
-    private Button login;
-    private Button next;
-    private EditText username;
-    private EditText password;
-    private SignInButton signin;
-    private GoogleSignInClient mGoogleSignInClient;
-    private int RC_SIGN_IN=0;
+
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        signin=findViewById(R.id.sign_in_button);
-        signin.setOnClickListener(v -> {
-            switch (v.getId()) {
-                case R.id.sign_in_button:
-                    signIn();
-                    break;
-                // ...
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home :
+                        selectorFragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_search :
+                        selectorFragment = new SearchFragment();
+                        break;
+
+                    case R.id.nav_add :
+                        selectorFragment = null;
+                        startActivity(new Intent(MainActivity.this , PostActivity.class));
+                        break;
+
+                    case R.id.nav_heart :
+                        selectorFragment = new NotificationFragment();
+                        break;
+
+                    case R.id.nav_profile :
+                        selectorFragment = new ProfileFragment();
+                        break;
+                }
+
+                if (selectorFragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , selectorFragment).commit();
+                }
+
+                return  true;
+
             }
         });
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        Bundle intent = getIntent().getExtras();
+        if (intent != null) {
+            String profileId = intent.getString("publisherId");
 
-    }
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+            getSharedPreferences("PROFILE", MODE_PRIVATE).edit().putString("profileId", profileId).apply();
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+            bottomNavigationView.setSelectedItemId(R.id.nav_profile);
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new HomeFragment()).commit();
         }
-    }
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            UpdateUI();
-            next =(Button)findViewById(R.id.next);
-            next.setOnClickListener(v -> {
-                if(v.getId()==R.id.next){
-                    Log.d("success","button recognized");
-                    //validation de password
-                    EditText passwordtxt=findViewById(R.id.editTextTextPassword2);
-                    Log.d("pass_saisie",passwordtxt.getText().toString());
-                    if(passwordtxt.getText().toString().equals("fuckoff3")){
-                        Log.d("success","password fine");
-                        //go to home page
-                        Intent intent=new Intent(this,HomePage.class);
-                        intent.putExtra("password",passwordtxt.getText().toString());
-                        startActivity(intent);
-                    }
-                    else Log.d("failure","password  not fine");
-
-
-
-
-                }
-            });
-
-
-
-
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("Error", "signInResult:failed code=" + e.getStatusCode());
-
-        }
-    }
-
-
-
-    public  void LoginHandler(View v){
-
-         username=findViewById(R.id.username);
-         password=findViewById(R.id.password);
-         String usernameInput=username.getText().toString();
-         String passwordInput=password.getText().toString();
-
-         //##################Login Logic Part#################################
-         if(usernameInput.equals("azerty") && passwordInput.equals("23"))   Log.d("success","u have logged in");
-         else Log.d("failure","u have not logged in");
-
-    }
-    private void UpdateUI(){
-        LinearLayout layout=findViewById(R.id.NewPassword);
-        LinearLayout loginform=findViewById(R.id.LoginForm);
-        signin.setVisibility(View.INVISIBLE);
-        loginform.setVisibility(View.INVISIBLE);
-        layout.setVisibility(View.VISIBLE);
     }
 }
